f04a8ae18b50165d9a72ee6b767774709e5cd21f
YanaUSA <usatiukiana@gmail.com>
2023-06-14 00:58:59 +0300

    add fetch logic for contacts

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,13 +1,39 @@
 import Heading from "../../components/Heading";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
-const Contacts = () => {
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  // const data = await response.json();
+
+  const data = null;
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { contacts: data },
+  };
+};
+
+const Contacts = ({ contacts }) => {
   return (
     <>
       <Head>
         <title>Contacts</title>
       </Head>
       <Heading text="Contacts list:" />
+      <ul>
+        {contacts &&
+          contacts.map(({ id, name, email }) => (
+            <li key={id}>
+              <strong>{name}</strong>({email})
+            </li>
+          ))}
+      </ul>
     </>
   );
 };
